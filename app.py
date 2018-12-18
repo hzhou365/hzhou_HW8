cd4146cdddfd8b68f94f4837aeaee5f3e1b732cd
hzhou365 <hzhou365@gmail.com>
2018-12-18 12:53:26 -0800

    2nd revision

--- app.py
+++ app.py
@@ -45,7 +45,9 @@ def welcome():
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/trip/\"startdate\"<br/>"
+        f"/api/v1.0/trip/\"startdate\"/\"enddate\"<br/>"
     )
 
 
@@ -128,6 +130,38 @@ def tobs():
     
     return jsonify(sorted_tob_list)
 
+
+def calc_temps(start_date, end_date):
+    """TMIN, TAVG, and TMAX for a list of dates.
+    
+    Args:
+        start_date (string): A date string in the format %Y-%m-%d
+        end_date (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+    
+    return session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+
+
+@app.route("/api/v1.0/trip/<start>")
+@app.route("/api/v1.0/trip/<start>/<end>")
+def trip(start,end="2017-08-23"):
+    """Return TMIN TAVG TMAX from the dataset"""
+    
+    exp_temps = calc_temps(start, end)
+    min_temp = exp_temps[0][0]
+    avg_temp = exp_temps[0][1]
+    max_temp = exp_temps[0][2]
+
+    list_of_temps=[]
+    list_of_temps.append(min_temp)
+    list_of_temps.append(avg_temp)
+    list_of_temps.append(max_temp)
+        
+    return jsonify(list_of_temps)
 
 if __name__ == '__main__':
     app.run(debug=True)
